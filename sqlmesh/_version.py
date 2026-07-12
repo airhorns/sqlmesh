f4eae0bc0511c164a85ae8f6c4b74109507de9f6
Harry Brundage <harry.brundage@gmail.com>
2026-07-12 18:33:35 -0400

    Pin fork package version for reproducible state metadata
    Signed-off-by: Harry Brundage <harry.brundage@gmail.com>

---
+++ sqlmesh/_version.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+__all__ = [
+    "__version__",
+    "__version_tuple__",
+    "version",
+    "version_tuple",
+    "__commit_id__",
+    "commit_id",
+]
+
+__version__ = version = "0.236.1.dev11"
+__version_tuple__ = version_tuple = (0, 236, 1, "dev11")
+__commit_id__ = commit_id = None
